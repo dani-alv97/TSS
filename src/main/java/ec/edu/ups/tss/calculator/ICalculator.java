4cbf6364c60193d0fb14c0ec68edbe966e69f276
Danii :) <charlialvarezbrau@hotmail.com>
2024-06-23 23:57:21 -0500

    Requisito 2

---
+++ src/main/java/ec/edu/ups/tss/calculator/ICalculator.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.tss.calculator;
+
+public interface ICalculator {
+
+	public int suma(int a, int b);
+	
+	public int resta(int a, int b);
+	
+	public int division(int a, int b);
+	
+	public int multiplicacion(int a, int b);
+	
+}
