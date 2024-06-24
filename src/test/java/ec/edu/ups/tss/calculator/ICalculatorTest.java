50f1a7897f381e00e6c3225ccaf0dfc4c5f416ea
Danii :) <charlialvarezbrau@hotmail.com>
2024-06-24 00:24:47 -0500

    Requisito 2: Servicios Web
    Cambio para refactorizar la prueba

--- src/test/java/ec/edu/ups/tss/calculator/ICalculatorTest.java
+++ src/test/java/ec/edu/ups/tss/calculator/ICalculatorTest.java
@@ -2,43 +2,47 @@ package ec.edu.ups.tss.calculator;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class ICalculatorTest {
 
+	ICalculator c = null;
+	
+	@Before
+	public void setUp() {
+		c = Mockito.mock(ICalculator.class);
+	}
+	
 	@Test
 	public void  suma() {
-		ICalculator c = Mockito.mock(ICalculator.class);
-		Mockito.when(c.suma(2, 3)).thenReturn(5);
 		
+		Mockito.when(c.suma(2, 3)).thenReturn(5);
 		assertEquals(5, c.suma(2, 3));
 		
 	}
 	
 	@Test
 	public void  resta() {
-		ICalculator c = Mockito.mock(ICalculator.class);
-		Mockito.when(c.resta(5, 2)).thenReturn(3);
 		
+		Mockito.when(c.resta(5, 2)).thenReturn(3);
 		assertEquals(3, c.resta(5, 2));
 		
 	}
 	
 	@Test
 	public void  division() {
-		ICalculator c = Mockito.mock(ICalculator.class);
-		Mockito.when(c.division(4, 2)).thenReturn(2);
 		
+		Mockito.when(c.division(4, 2)).thenReturn(2);
 		assertEquals(2, c.division(4, 2));
 		
 	}
 	
 	@Test
 	public void  multiplicacion() {
-		ICalculator c = Mockito.mock(ICalculator.class);
-		Mockito.when(c.multiplicacion(5, 2)).thenReturn(10);
 		
+		Mockito.when(c.multiplicacion(5, 2)).thenReturn(10);
 		assertEquals(10, c.multiplicacion(5, 2));
 		
 	}
